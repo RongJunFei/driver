dfcd7dc4a3e288e256df7f20d05ba13fbf6df741
Rong <rjfsg123@163.com>
2016-02-17 20:55:23 +0800

    Use module_param/MODULE_PARAMDESC

--- hello.c
+++ hello.c
@@ -1,13 +1,36 @@
 #include <linux/cdev.h>
+:q
 #include <linux/module.h>
+#include <linux/moduleparam.h>
+#include <linux/kernel.h>
 
+static int myinit = 222;
+static short int myshort = 111;
+static long int mylog = 333;
+static char * mystring = "My string";
 
-int __init hello_init(void){
+module_param(myinit, int, 0400);
+MODULE_PARM_DESC(myinit, "An integer");
+
+module_param(myshort, short, 0400);
+MODULE_PARM_DESC(myshort, "A short integer");
+
+module_param(mylog, long, 0000);
+MODULE_PARM_DESC(mylog, "A long integer");
+
+module_param(mystring, charp, 0000);
+MODULE_PARM_DESC(mystring, "A character string");
+
+static int __init hello_init(void){
 	printk("Hello Linux device!\n");
+	printk("### myinit = %d \n", myinit);
+	printk("### myshort = %hd \n", myshort);
+	printk("### mylong = %ld \n", mylog);
+	printk("### mystring = %s \n", mystring);
 	return 0;
 }
 
-void __exit hello_exit(void){
+static void __exit hello_exit(void){
 	printk("Bye Linux device!\n");
 }
 
